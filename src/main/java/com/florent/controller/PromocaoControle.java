5f831dc31389eb0e6590112d8815b2887dc2fa25
Miguelito2926 <juniorprol@hotmail.com>
2022-03-18 12:19:07 -0300

    Repositorys

---
+++ src/main/java/com/florent/controller/PromocaoControle.java
@@ -0,0 +1,5 @@
+package com.florent.controller;
+
+public class PromocaoControle {
+
+}
--- src/main/java/com/florent/repository/PromocaoRepository.java
+++ src/main/java/com/florent/repository/PromocaoRepository.java
@@ -3,12 +3,9 @@ package com.florent.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.florent.model.Cliente;
+import com.florent.model.Promocao;
 
 @Repository
-public interface ClienteRepository extends JpaRepository<Cliente,Long> {
+public interface PromocaoRepository extends JpaRepository<Promocao, Long> {
 
-
-	
 }
-
